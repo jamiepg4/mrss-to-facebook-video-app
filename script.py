3116dc40eff2840bd3f5faffa1215a42d7dea999
Daniel Bachhuber <d@danielbachhuber.com>
2015-08-25 07:25:46 -0700

    Upload videos to Facebook (needs authorization)

--- script.py
+++ script.py
@@ -1,16 +1,23 @@
 import feedparser
+import json
 import os
+import requests
 from honcho import environ
 
 def parse_videos_from_feed():
     """
-    Injest MRSS feed into local scope
+    Injest MRSS feed into local scope; format videos to FB upload spec
     """
 
     data = feedparser.parse(os.getenv('MTFV_MRSS_URL'))
     videos = []
     for i, video in enumerate(data.entries):
-        videos.append( video )
+        to_append = {}
+        to_append['file_url'] = video['media_content'][0]['url']
+        to_append['file_size'] = video['media_content'][0]['filesize']
+        to_append['title'] = video['title']
+        # to_append['thumb'] = video['media_thumbnail']['url'] # Requires downloading and including in post body
+        videos.append( to_append )
     return videos
 
 def update_env(filename='.env'):
@@ -27,6 +34,19 @@ def update_env(filename='.env'):
     for k, v in envvars.items():
         os.environ[k] = v
 
+def upload_video_to_facebook(video):
+    """
+    Uploads a given video to Facebook Graph API
+    """
+
+    request_url = 'https://graph-video.facebook.com/v2.3/%s/videos' % ( os.getenv('MTFV_FACEBOOK_ENTITY_ID') )
+    print request_url
+    print video
+    response = requests.post( request_url, video )
+    print response
+
+
 update_env();
 videos = parse_videos_from_feed();
-print videos
+for video in videos:
+    upload_video_to_facebook( video )
